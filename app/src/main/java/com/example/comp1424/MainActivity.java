bb4e7300c1ebb7a8b51ea8c0c9df1d57b569c5be
DMukeshkumar <harsudadhaval32@gmail.com>
2022-11-08 12:10:02 +0000

    //added bottom navigation panel and different fragments for different pages

--- app/src/main/java/com/example/comp1424/MainActivity.java
+++ app/src/main/java/com/example/comp1424/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.comp1424;
 
+import java.io.*;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import com.example.comp1424.*;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.person);
+
+    }
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.person:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+                return true;
+
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
+                return true;
+
+            case R.id.search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, thirdFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
